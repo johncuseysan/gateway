ac4bc3c8081eccde930c2050895d0b0867e755e0
johncuseysan <johncuseysan@gmail.com>
2022-04-20 09:49:49 -0400

    init commit

--- src/main/java/org/cusey/john/routes/ServiceRouteConfiguration.java
+++ src/main/java/org/cusey/john/routes/ServiceRouteConfiguration.java
@@ -23,17 +23,7 @@ public class ServiceRouteConfiguration {
 	public RouteLocator gatewayRoutes(RouteLocatorBuilder builder, CustomFilter custom, ObjectMapper objectMapper) {
 		
 		RouteLocator route = null;
-		
-		
-		//http://localhost:8082/employee/message -> http://localhost:8084/employee/message (third-service)
-		//JUST to test the ROUTER to see if it is working
-		/*
-		route = builder.routes() 
-		  			.route(r -> r.path("/employee/message")
-		 			.uri("http://localhost:8084/")) 
-		 			.build();
-		 */
-		
+
 		
 		//http://localhost:8082/cornell/api/student/search -> http://localhost:8083/cornell/api/student/search
 		route = builder.routes()
